e17f465a044702cca9b6d05412e8b634bce63ae2
Dawid Femin <w65311@student.wsiz.edu.pl>
2022-03-02 22:57:53 +0100

    Created basic structure and questions

---
+++ main.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        questions: 'Who has won the Champions League final in season 2020/2021',
+        a: 'Chelsea',
+        b: 'Manchester City',
+        c: 'Manchester United',
+        d: 'Real Madrid',
+        correct: 'a'
+    }, {
+        questions: 'Who won the first Ballon dOr',
+        a: 'Cristiano Ronaldo',
+        b: 'Leo Messi',
+        c: 'Diego Maradona',
+        d: 'Stanley Matthews',
+        correct: 'd'
+    }, {
+        questions: 'Who has the most European Cup/Champions League victories?',
+        a: 'Chelsea',
+        b: 'FC Barcelona',
+        c: 'Manchester United',
+        d: 'Real Madrid',
+        correct: 'd'
+    }, {
+        questions: 'How many clubs did David Beckham play for during his career?',
+        a: '3',
+        b: '4',
+        c: '6',
+        d: '7',
+        correct: 'c'
+    }, {
+        questions: 'In what year did Arsenal leave Highbury?',
+        a: '2010',
+        b: '2006',
+        c: '2016',
+        d: '2002',
+        correct: 'b'
+    }
+]
