a3ca64e352518dc0bb72f4726585157dad229001
Dawid Femin <w65311@student.wsiz.edu.pl>
2022-03-03 23:50:57 +0100

    Created all basic function

--- main.js
+++ main.js
@@ -34,5 +34,70 @@ const quizData = [
         c: '2016',
         d: '2002',
         correct: 'b'
-    }
-]
+    },
+];
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('text-a');
+const b_text = document.getElementById('text-b');
+const c_text = document.getElementById('text-c');
+const d_text = document.getElementById('text-d');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswer();
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerHTML = currentQuizData.questions;
+    a_text.innerHTML = currentQuizData.a;
+    b_text.innerHTML = currentQuizData.b;
+    c_text.innerHTML = currentQuizData.c;
+    d_text.innerHTML = currentQuizData.d;
+}
+
+function getSelected() {
+
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }        
+    });
+
+    return answer;
+}
+
+function deselectAnswer() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener('click', () => {
+
+    const answer = getSelected();
+
+        if(answer) {
+            if(answer === quizData[currentQuiz].correct) {
+                score++;
+            }
+            
+            currentQuiz++;
+
+            if(currentQuiz < quizData.length) {
+                loadQuiz();
+            }
+            else {
+                quiz.innerHTML = `<h2> You answered correctly at ${score}/${quizData.length} questions</h2>`
+            }
+        } 
+
+});
